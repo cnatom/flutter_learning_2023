4f6ed9b045163f8d58ab84a3301d55d08d544132
CUMT-Atom <cnatom@foxmail.com>
2023-11-02 15:50:31 +0800

    🎉init(app):初始化项目

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("我的第一款App"),
+      ),
+      body: Center(
+        child: Text("我是Body部分！"),
+      ),
+    );
+  }
+}
+
